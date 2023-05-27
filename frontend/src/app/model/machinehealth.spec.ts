c49d60cab2faf79d60360a7fffebe4495a5d7b17
Mr. Hopper <81442557+Hoppering@users.noreply.github.com>
2023-05-27 17:03:06 +0300

    Delete frontend directory

--- frontend/src/app/model/machinehealth.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Machinehealth } from './machinehealth';
-
-describe('Machinehealth', () => {
-  it('should create an instance', () => {
-    expect(new Machinehealth("hope", "dope")).toBeTruthy();
-  });
-});
